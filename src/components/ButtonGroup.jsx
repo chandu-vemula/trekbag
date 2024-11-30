9289596eaca029dd449b5bffc7e1d8ebd534fe02
chandu-vemula <chanduvemula1996@gmail.com>
2024-12-01 03:55:00 +0530

    Use map to render buttons

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -1,12 +1,27 @@
 import Button from "./Button";
 
 function ButtonGroup() {
+  const secondaryButtons = [
+    "Mark as complete",
+    "Mark as incomplete",
+    "Reset to initial",
+    "Remove all items",
+  ];
+
   return (
     <section className="button-group">
-      <Button type="secondary">Mark as complete</Button>
+      {/* <Button type="secondary">Mark as complete</Button>
       <Button type="secondary">Mark as incomplete</Button>
       <Button type="secondary">Reset to initial</Button>
-      <Button type="secondary">Remove all items</Button>
+      <Button type="secondary">Remove all items</Button> */}
+
+      {secondaryButtons.map((text) => {
+        return (
+          <Button key={text} type="secondary">
+            {text}
+          </Button>
+        );
+      })}
     </section>
   );
 }
