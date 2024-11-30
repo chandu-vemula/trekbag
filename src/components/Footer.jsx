2a40df46162624bdb0d1650c07fed7e0e352b7f4
chandu-vemula <chanduvemula1996@gmail.com>
2024-12-01 02:46:14 +0530

    Add the structure and omponents to app

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <footer>
+      <small>&copy; 2025. Copyright by Chandu Vemula</small>
+      <p>
+        Version <b>1.5</b>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
